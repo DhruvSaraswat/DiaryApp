d8480c645907478b0b5966f7d543f977cf89376a
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-02-20 16:54:25 +0530

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by Dhruv Saraswat on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
