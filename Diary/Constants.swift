6d9e56475e1a99cfa1a68fe5347121523bb4394c
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-03-03 01:12:41 +0530

    Removed an unused Constant

--- Diary/Constants.swift
+++ Diary/Constants.swift
@@ -13,6 +13,5 @@ struct Constants {
         static let textColor = Color("textColor")
         static let selectedDateCircleColor = UIColor(red: 243 / 255, green: 178 / 255, blue: 134 / 255, alpha: 1)
         static let weekdayColor = UIColor(red: 207 / 255, green: 195 / 255, blue: 178 / 255, alpha: 1)
-        static let daysOutsideMonthColor = UIColor(red: 242 / 255, green: 242 / 255, blue: 242 / 255, alpha: 1)
     }
 }
