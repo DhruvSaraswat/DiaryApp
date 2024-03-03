2f8cb9368d803c767cc5648a57cc6384fb05e726
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-03-03 12:08:48 +0530

    This commit removes the now-redundant struct NetworkImage

--- Diary/HomeView.swift
+++ Diary/HomeView.swift
@@ -46,25 +46,6 @@ struct HomeView: View {
     }
 }
 
-// TODO: Use `AsyncImage` instead of synchronously downloading the image
-struct NetworkImage: View {
-    let url: URL?
-
-    var body: some View {
-        if let url = url,
-           let data = try? Data(contentsOf: url),
-           let uiImage = UIImage(data: data) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-        } else {
-            Image(systemName: "person.circle.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-        }
-    }
-}
-
 #Preview {
     HomeView()
         .environmentObject(AuthenticationViewModel())
