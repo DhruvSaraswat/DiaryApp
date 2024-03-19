7841c0cfdfe561e1b6aae2148ff325ef671e3d62
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-03-19 11:44:13 +0530

    This commit contains one approach to open the Diary entry from the diary entry list view

--- Diary/Modules/DiaryEntryList/DiaryEntryListView.swift
+++ Diary/Modules/DiaryEntryList/DiaryEntryListView.swift
@@ -13,35 +13,46 @@ struct DiaryEntryListView: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
     @Query(Persistence.fetchDescriptor) private var items: [DiaryEntryItem]
+    @State private var showDiaryEntryView: Bool = false
+    @State private var tappedOnDiaryEntryItem: DiaryEntryItem = DiaryEntryItem(title: "", story: "", diaryTimestamp: 0, diaryDate: "", createdAtTimestamp: 0, lastEditedAtTimestamp: 0)
 
     var body: some View {
-        VStack {
-            Text("All Diary Entries")
-                .font(.title)
+        if showDiaryEntryView {
+            DiaryEntryView()
+                .environmentObject(DiaryEntryViewModel(diaryEntryItem: tappedOnDiaryEntryItem))
+        } else {
+            VStack {
+                Text("All Diary Entries")
+                    .font(.title)
 
-            List {
-                ForEach(items) { item in
-                    DiaryEntryRow(diaryDate: item.diaryDate, title: item.title)
-                    .listRowSeparator(.hidden)
-                    .listRowBackground(
-                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-                            .foregroundStyle(Color(UIColor(red: 242 / 255, green: 231 / 255, blue: 225 / 255, alpha: 1)))
-                            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: -20))
-                    )
+                List {
+                    ForEach(items) { item in
+                        DiaryEntryRow(diaryDate: item.diaryDate, title: item.title)
+                            .listRowSeparator(.hidden)
+                            .listRowBackground(
+                                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                    .foregroundStyle(Color(UIColor(red: 242 / 255, green: 231 / 255, blue: 225 / 255, alpha: 1)))
+                                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: -20))
+                            )
+                            .onTapGesture {
+                                tappedOnDiaryEntryItem = item
+                                showDiaryEntryView.toggle()
+                            }
+                    }
                 }
+                .listStyle(.plain)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
             }
-            .listStyle(.plain)
-            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button(action: {
-                    dismiss()
-                }, label: {
-                    Image(systemName: "arrow.backward")
-                        .tint(Constants.Colors.backArrowTint)
-                })
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                        Image(systemName: "arrow.backward")
+                            .tint(Constants.Colors.backArrowTint)
+                    })
+                }
             }
         }
     }
