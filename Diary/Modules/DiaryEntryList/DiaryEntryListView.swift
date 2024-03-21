f57cf9a0c5d3980d4ad3e18ce65924a10d1573c5
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-03-21 15:28:41 +0530

    Added the swipe-to-delete functionality in the DiaryEntryListView

--- Diary/Modules/DiaryEntryList/DiaryEntryListView.swift
+++ Diary/Modules/DiaryEntryList/DiaryEntryListView.swift
@@ -12,6 +12,9 @@ import SwiftData
 struct DiaryEntryListView: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var calendarViewModel: CalendarViewModel
+    @EnvironmentObject private var authViewModel: AuthenticationViewModel
+    @EnvironmentObject private var homeViewModel: HomeViewModel
     @Query(Persistence.fetchDescriptor) private var items: [DiaryEntryItem]
 
     var body: some View {
@@ -25,6 +28,7 @@ struct DiaryEntryListView: View {
                 ForEach(items) { item in
                     DiaryEntryRow(diaryEntryItem: item)
                 }
+                .onDelete(perform: deleteDiaryItems)
             }
             .listStyle(.plain)
             .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
@@ -38,6 +42,19 @@ struct DiaryEntryListView: View {
                     Image(systemName: "arrow.backward")
                         .tint(Constants.Colors.backArrowTint)
                 })
+            }
+        }
+    }
+
+    func deleteDiaryItems(at offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+                homeViewModel.deleteDiaryEntry(userId: authViewModel.getUserId(), diaryTimestamp: items[index].diaryTimestamp, completion: nil)
+            }
+            /// hide the dot below all the calendar dates for which a diary entry has been deleted
+            DispatchQueue.main.async {
+                calendarViewModel.calendar.reloadData()
             }
         }
     }
